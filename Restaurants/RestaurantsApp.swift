7d401b489346b21e1e94e8b5cc344bf8c2bd9e2f
Kyoka-Yamamoto <annforstudyabroad@gmail.com>
2023-11-19 21:08:04 -0400

    Initial Commit

---
+++ Restaurants/RestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsApp.swift
+//  Restaurants
+//
+//  Created by Kyoka Yamamoto on 2023-11-19.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
